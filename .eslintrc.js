a4ece44952302550c407c8c6bd2500655d41ada7
donghzh <donghzh@ygemail.com>
2020-04-20 15:05:55 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   env: {
     es6: true,
     node: true,
   },
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        trailingComma: "all",
-        arrowParens: "always",
-        printWidth: 100,
-        semi: true,
-        
-      },
-    ],
+    "prettier/prettier": ["error"],
     "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-function-return-type":
+      "off",
   },
   plugins: ["prettier"],
-}
+};
